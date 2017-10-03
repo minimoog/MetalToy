dca33d3a2a1825f197b1413bb0d98ea2f473f471
Toni Jovanoski <minimoog77@gmail.com>
2017-10-04 01:37:51 +0200

    Adjust codeview when keyboard shows.

--- MetalToy/CodeViewController.swift
+++ MetalToy/CodeViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class CodeViewController: UIViewController {
     
     @IBOutlet weak var codeView: UITextView!
+    @IBOutlet weak var keyboardHeightLayoutConstraint: NSLayoutConstraint!
     weak var metalViewController: MetalViewController?
     
     override func viewDidLoad() {
@@ -18,6 +19,14 @@ class CodeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         codeView.text = DefaultFragmentShader
+        
+        //keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     @objc func onPlayButtonTapped(sender: UIBarButtonItem) {
@@ -35,6 +44,25 @@ class CodeViewController: UIViewController {
                 metalViewController.mtkView.isPaused = true
             }
         }
+    }
+    
+    @objc func keyboardWasShown(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.keyboardHeightLayoutConstraint.constant = keyboardFrame.size.height + 20
+        })
+    }
+    
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.keyboardHeightLayoutConstraint.constant = 20
+        })
+    }
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        
     }
     
     override func didReceiveMemoryWarning() {
