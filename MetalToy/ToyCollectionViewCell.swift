727d152ca9dadabfd067f1295cdbf0ff9a88d6ae
minimoog77 <minimoog77@gmail.com>
2018-01-23 00:39:39 +0100

    Initial implementation of ToyCollectionViewController

---
+++ MetalToy/ToyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ToyCollectionViewCell.swift
+//  MetalToy
+//
+//  Created by minimoog on 1/23/18.
+//  Copyright © 2018 Toni Jovanoski. All rights reserved.
+//
+
+import UIKit
+
+class ToyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var toyNameLabel: UILabel!
+    
+}
