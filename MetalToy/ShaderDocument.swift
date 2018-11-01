876343a46dedd6bd32d71d93be3338c93c04eeee
minimoog77 <minimoog77@gmail.com>
2018-11-02 00:32:11 +0100

    Simplify shader document.

--- MetalToy/ShaderDocument.swift
+++ MetalToy/ShaderDocument.swift
@@ -10,14 +10,6 @@ import UIKit
 
 class ShaderDocument: UIDocument {
     var shaderInfo: ShaderInfo?
-    var thumbnail: UIImage?
-    
-    enum SubDocumentType: String {
-        case shaderinfo = "shaderinfo.json"
-        case thumbnail = "thumbnail.png"
-        case shader = "shader.txt"  //old format
-        case name = "name.txt"      //old format
-    }
     
     override init(fileURL url: URL) {
         super.init(fileURL: url)
@@ -31,54 +23,20 @@ class ShaderDocument: UIDocument {
     }
     
     override func contents(forType typeName: String) throws -> Any {
-        if let shaderInfo = shaderInfo, let thumbnail = thumbnail {
+        if let shaderInfo = shaderInfo {
             guard let jsonData = encodeToJsonData(shaderInfo: shaderInfo) else { return Data() }
             
-            let shaderInfoFileWrapper = FileWrapper(regularFileWithContents: jsonData)
-            
-            guard let imageData = UIImagePNGRepresentation(thumbnail) else { return Data() }
-            let thumbnailFileWrapper = FileWrapper(regularFileWithContents: imageData)
-            
-            let dirWrapper = FileWrapper(directoryWithFileWrappers: [SubDocumentType.shaderinfo.rawValue: shaderInfoFileWrapper,
-                                                                     SubDocumentType.thumbnail.rawValue: thumbnailFileWrapper])
-            return dirWrapper
-        } else {
-            return Data()
+            return jsonData as Any
         }
+        
+        return Data()
     }
     
     override func load(fromContents contents: Any, ofType typeName: String?) throws {
-        if let userContents = contents as? FileWrapper {
-            if userContents.isDirectory {
-                if  let dirWrapper = userContents.fileWrappers,
-                    let shaderInfoFileWrapper = dirWrapper[SubDocumentType.shaderinfo.rawValue],
-                    let thumbnailFileWrapper = dirWrapper[SubDocumentType.thumbnail.rawValue],
-                    let shaderInfoData = shaderInfoFileWrapper.regularFileContents,
-                    let imageData = thumbnailFileWrapper.regularFileContents {
-                        shaderInfo = decodeFromJsonData(data: shaderInfoData)
-                        thumbnail = UIImage(data: imageData)
-                } else {
-                    // read from old format
-                    if let dirWrapper = userContents.fileWrappers,
-                        let shaderTextFileWrapper = dirWrapper[SubDocumentType.shader.rawValue],
-                        let nameFileWrapper = dirWrapper[SubDocumentType.name.rawValue],
-                        let shaderTextData = shaderTextFileWrapper.regularFileContents,
-                        let thumbnailFileWrapper = dirWrapper[SubDocumentType.thumbnail.rawValue],
-                        let imageData = thumbnailFileWrapper.regularFileContents,
-                        let nameData = nameFileWrapper.regularFileContents {
-                        
-                        shaderInfo = ShaderInfo(name: String(data: nameData, encoding: .utf8)!,
-                                                             fragment: String(data: shaderTextData, encoding: .utf8)!,
-                                                             textures: [String]())
-                        
-                        //remove old data
-                        userContents.removeFileWrapper(nameFileWrapper)
-                        userContents.removeFileWrapper(shaderTextFileWrapper)
-                        
-                        thumbnail = UIImage(data: imageData)
-                    }
-                }
-            }
+        guard let data = contents as? Data else {
+            fatalError("\(contents) is not an instance of Data")
         }
+        
+        shaderInfo = decodeFromJsonData(data: data)
     }
 }
