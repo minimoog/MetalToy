35b29563fb07ff6b3234956c40510ffa82843c44
Toni Jovanoski <minimoog77@gmail.com>
2018-12-18 22:34:27 +0100

    Little cleanup.

--- MetalToy/EditorNavigationController.swift
+++ MetalToy/EditorNavigationController.swift
@@ -13,8 +13,6 @@ class EditorNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,15 +25,4 @@ class EditorNavigationController: UINavigationController {
             editorViewController.document = document
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
